ac66c7402bddf312971b861a351e654bc2ff2680
Tommi Mäkitalo <tommi@tntnet.org>
2013-10-08 15:48:07 +0200

    implement search by date

--- src/controller/suche.cpp
+++ src/controller/suche.cpp
@@ -1,3 +1,4 @@
+#include <sstream>
 #include <tnt/component.h>
 #include <tnt/componentfactory.h>
 #include <tnt/httprequest.h>
@@ -10,6 +11,21 @@ log_define("suche.controller")
 
 namespace
 {
+  cxxtools::Date str2date(const std::string& s)
+  {
+    std::istringstream in(s);
+    char ch;
+    unsigned n1, n2, n3;
+    in >> n1 >> ch >> n2 >> ch >> n3;
+    if (!in)
+      throw std::runtime_error("invalid date: \"" + s + '"');
+
+    if (ch == '-')  // Format: jjjj-mm-dd
+      return cxxtools::Date(n1, n2, n3);
+    else
+      return cxxtools::Date(n3, n2, n1);  // gehen wir mal mutig von dd.mm.jjjj aus
+  }
+
   class sucheController : public tnt::Component
   {
     public:
@@ -38,10 +54,19 @@ namespace
 
       std::string q        = qparam.arg<std::string>("q");
       std::string bezirk   = qparam.arg<std::string>("bezirk");
+
       std::string von_from = qparam.arg<std::string>("von_from");
       std::string von_to   = qparam.arg<std::string>("von_to");
 
-      strassenfestResult = manager.search(q, bezirk, cxxtools::Date(), cxxtools::Date(), cxxtools::Date(), 20, 1);
+      cxxtools::Date from;
+      if (!von_from.empty())
+        from = str2date(von_from);
+
+      cxxtools::Date to;
+      if (!von_to.empty())
+        to = str2date(von_to);
+
+      strassenfestResult = manager.search(q, bezirk, from, to, cxxtools::Date(), 20, 1);
     }
 
     return DECLINED;
